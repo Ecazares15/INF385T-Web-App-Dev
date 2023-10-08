fde90499572f730b872133611d1c73673b01f2f7
Eduardo Cazares <ecazares31@gmail.com>
2023-10-07 21:01:26 -0500

    Cleaned up a bit and made some starting pages to work with

--- wander_world_ui/src/components/Register/Register.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Register from './Register';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Register />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
