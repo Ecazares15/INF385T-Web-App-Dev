1a255d03e82557553c95381c15ea4942cf059b0b
Eduardo Cazares <ecazares31@gmail.com>
2023-10-09 10:24:53 -0500

    Added basic starting login and register pages

--- wander_world_ui/src/App.tsx
+++ wander_world_ui/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Feed from './pages/Feed';
-import Community from './pages/Community';
-import Profile from './pages/Profile';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Feed from "./pages/Feed";
+import Community from "./pages/Community";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </LocalizationProvider>
   );
 }
 
--- wander_world_ui/src/pages/Login.tsx
+++ wander_world_ui/src/pages/Login.tsx
@@ -1,11 +1,30 @@
-
+import React from 'react'
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Container from '@mui/material/Container';
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+      <Container className='mt-5' sx={{ display: "flex", justifyContent: "center" }}>
+        <Paper className="w-50 h-50 text-center" elevation={1}>
+          <div className="m-5">
+            <div className='pb-3'>
+              <h1>Login</h1>
+            </div>
+            <div className='pb-3'>
+              <TextField required label="Username" type="text" />
+            </div>
+            <div className='pb-3'>
+              <TextField required label="Password" type="text" />
+            </div>
+            <div>
+              <Button variant="contained">Login</Button>
+            </div>
+          </div>
+        </Paper>
+      </Container>
+    );
 }
 
 export default Login
