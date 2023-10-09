8a77f67fdc058282506dbff7992fdaf030e92923
Eduardo Cazares <ecazares31@gmail.com>
2023-10-09 09:44:46 -0500

    Fixed routes, they now work properly

--- wander_world_ui/src/App.tsx
+++ wander_world_ui/src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Feed from './pages/Feed';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
