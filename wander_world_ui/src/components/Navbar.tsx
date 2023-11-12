ac616b875f1dcd199dae8d227439877a253826c8
jasmineywang <jasminewang@utexas.edu>
2023-11-12 11:43:11 -0600

    passwordinput and global colors

--- wander_world_ui/src/components/Navbar.tsx
+++ wander_world_ui/src/components/Navbar.tsx
@@ -1,49 +1,41 @@
-import React, { FormEvent } from 'react';
-import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap'
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
-import musicMapLogo from '../assets/musicmaplogo.svg'
 import wanderWorldLogo from '../assets/wanderworld_logo.svg'
 
 const NavBar: React.FC<{}> = () => {
 
     return (
-        <div>
-            <Navbar variant="dark" expand="lg" style={{backgroundColor: "#1f4098"}}>
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img src={wanderWorldLogo} 
-                            height="30"
-                            className="d-inline-block align-top"
-                            alt="logo"/>{'  '}
-                        WanderWorld
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            
-                            <LinkContainer to="/about">
-                                <Nav.Link>About</Nav.Link>
-                            </LinkContainer>
+        <Navbar variant="dark" expand="lg" style={{backgroundColor: "#1f4098"}}>
+            <Container>
+                <Navbar.Brand href="/">
+                    <img src={wanderWorldLogo} 
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="logo"/>{'  '}
+                    WanderWorld
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
 
-                            <LinkContainer to="/feed">
-                                <Nav.Link>Feed</Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/feed">
+                            <Nav.Link>Feed</Nav.Link>
+                        </LinkContainer>
 
-                            <LinkContainer to="/community">
-                                <Nav.Link>Community</Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/community">
+                            <Nav.Link>Community</Nav.Link>
+                        </LinkContainer>
 
-                            
-                            <LinkContainer to="/profile">
-                                <Nav.Link>Profile</Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/profile">
+                            <Nav.Link>Profile</Nav.Link>
+                        </LinkContainer>
 
-                        </Nav>
+                    </Nav>
 
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
--- wander_world_ui/src/pages/Login.tsx
+++ wander_world_ui/src/pages/Login.tsx
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Container from '@mui/material/Container';
+import PasswordInput from '../components/PasswordInput';
 
 function Login() {
-    return (
-      <Container className='mt-5' sx={{ display: "flex", justifyContent: "center" }}>
-        <Paper className="w-50 h-50 text-center" elevation={1}>
-          <div className="m-5">
-            <div className='pb-3'>
-              <h1>Login</h1>
-            </div>
-            <div className='pb-3'>
-              <TextField required label="Username" type="text" />
-            </div>
-            <div className='pb-3'>
-              <TextField required label="Password" type="text" />
-            </div>
-            <div>
-              <Button variant="contained">Login</Button>
-            </div>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    // handle authentication
+
+  };
+
+  return (
+    <Container className='mt-5' sx={{ display: "flex", justifyContent: "center" }}>
+      <Paper className="w-50 h-50 text-center" elevation={1}>
+        <div className="m-5">
+          <div className='pb-3'>
+            <h1>Login</h1>
           </div>
-        </Paper>
-      </Container>
-    );
+          <div className='pb-3'>
+            <TextField
+              required
+              label="Username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className='pb-3'>
+            <PasswordInput setPassword={setPassword} />
+          </div>
+          <div>
+            <Button variant="contained" onClick={handleLogin}>Login</Button>
+          </div>
+        </div>
+      </Paper>
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
